b61001f6af8aaf728c42fbaa2747728a593b2947
tejal-ui <tejalkhandave.ui@gmail.com>
2024-08-08 01:34:34 +0530

    flag ex

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Counter from './counter/Counter';
+import Flag from './flag/Flag';
 import UserDetails from './Props/UserDetails';
 
 function App() {
   return (
     <div className="App">
-      <UserDetails greeting="Hello" name="Tejal"></UserDetails>
-      <Counter></Counter>
+      {/* <UserDetails greeting="Hello" name="Tejal"></UserDetails> */}
+      {/* <Counter></Counter> */}
+      <Flag></Flag>
     </div>
   );
 }
---
+++ src/flag/Flag.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import './flag.css';
+export default function Flag() {
+  const [currentSate, changeState] = useState('online');
+  const on = () => {
+    changeState('Online');
+  };
+  const off = () => {
+    changeState('Offline');
+  };
+  return (
+    <div className="flag">
+      <h2>I'm {currentSate}</h2>
+      <button className="btn on" onClick={on}>
+        On
+      </button>
+      <button className="btn off" onClick={off}>
+        Off
+      </button>
+      {/* 2nd way to add function in button when onClick  */}
+      {/* <button
+        className="off"
+        onClick={() => {
+          changeState('Offline');
+        }}
+      >
+        Off
+      </button> */}
+    </div>
+  );
+}
