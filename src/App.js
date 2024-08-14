f95e1660ad36f508d180e722acb4b7d18e97aa03
tejal-ui <tejalkhandave.ui@gmail.com>
2024-08-14 15:24:27 +0530

    Added todo list

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
       {/* <Flag></Flag> */}
       {/* <FlagBoolean></FlagBoolean> */}
       {/* <UserProfile></UserProfile> */}
-      {/* <ToDoList></ToDoList> */}
-      <UserList></UserList>
+      <ToDoList></ToDoList>
+      {/* <UserList></UserList> */}
     </div>
   );
 }
--- src/todolist/ToDoList.js
+++ src/todolist/ToDoList.js
@@ -1,8 +1,17 @@
 import { useState } from 'react';
 
 export default function ToDoList() {
-  const [todos, setTodos] = useState(['Task1', 'Task2', 'Task3', 'Task4']);
+  // const [todos, setTodos] = useState(['Task1', 'Task2', 'Task3', 'Task4']);
+  const [todos, setTodos] = useState([
+    { taskId: 1, name: 'Task1' },
+    { taskId: 2, name: 'Task2' },
+    { taskId: 3, name: 'Task3' },
+  ]);
+  const [text, setText] = useState('');
 
+  const onInputChange = (e) => {
+    setText(e.target.value);
+  };
   // const getTodos = () => {
   //   const todoElements = [];
   //   todos.forEach((todo) => {
@@ -20,10 +29,27 @@ export default function ToDoList() {
       {/* <p>{todos[0]}</p>
       <p>{todos[1]}</p>
        */}
-      {/* 3rd way to access elements from an array **Mostly Used in programs**/}
-      {todos.map((todo) => (
-        <p>{todo}</p>
-      ))}
+      {/* 3rd way to access elements from an array **Mostly Used in programs is using map**/}
+      <input placeholder="Enter Todo" value={text} onChange={onInputChange} />
+      <button
+        onClick={() => {
+          // 1.solution one
+          if (text.length > 3 && text.includes('Task')) {
+            console.log([...todos, { name: text, taskId: todos.length + 1 }]);
+            setTodos([...todos, { name: text, taskId: todos.length + 1 }]);
+          }
+          //2.solution two
+          // const updatedTodos = [...todos];
+          // updatedTodos.push({ name: text, taskId: todos.length + 1 });
+          // console.log(updatedTodos);
+          // setTodos(updatedTodos);
+        }}
+      >
+        Add
+      </button>
+      {todos.map((todo) => {
+        return <p key={todo.taskId}>{` ${todo.name}`}</p>;
+      })}
     </div>
   );
 }
